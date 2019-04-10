bf6009a7e24c7c516c22cf02857dea46190ee00a
victorgarciasisi <45923592+victorgarciasisi@users.noreply.github.com>
2019-04-10 13:10:37 +0200

    Initial
    Initial

---
+++ send.php
@@ -0,0 +1,33 @@
+<?php
+
+//librerias
+  require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+$mail->IsSMTP();
+
+//Configuracion servidor mail
+$mail->From = "mail@gmail.com"; //remitente
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls'; //seguridad
+$mail->Host = "smtp.gmail.com"; // servidor smtp
+$mail->Port = 587; //puerto
+$mail->Username ='mail@gmail.com'; //nombre usuario
+$mail->Password = 'password'; //contraseña
+
+//Agregar destinatario
+$mail->AddAddress($_POST['email']);
+$mail->Subject = $_POST['subject'];
+$mail->Body = $_POST['message'];
+
+//Avisar si fue enviado o no y dirigir al index
+if ($mail->Send()) {
+    echo'<script type="text/javascript">
+           alert("Enviado Correctamente");
+        </script>';
+} else {
+    echo'<script type="text/javascript">
+           alert("NO ENVIADO, intentar de nuevo");
+        </script>';
+}
